6a6ce2d0e2f2dc596707beb46eccc08d6fc5d910
Herve Obejero <herve.obejero@gmail.com>
2025-12-23 03:53:47 +0100

    Add OffloadView and MediaView with professional layouts

---
+++ src/Veriflow.UI/Views/MediaView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Veriflow.UI.Views;
+
+public partial class MediaView : UserControl
+{
+    public MediaView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/Veriflow.UI/Views/OffloadView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Veriflow.UI.Views;
+
+public partial class OffloadView : UserControl
+{
+    public OffloadView()
+    {
+        InitializeComponent();
+    }
+}
